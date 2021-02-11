4e2c4bdea133d9630ac342904513a8d02e271636
Michael Condori Mavila <majayesh2017@gmail.com>
2021-02-11 16:17:47 -0500

    Estudiante 04 - Michael Condori Mavila

--- Final/ExamenDAMFinal/app/src/main/java/com/example/examendamfinal/Formulario.java
+++ Final/ExamenDAMFinal/app/src/main/java/com/example/examendamfinal/Formulario.java
@@ -3,9 +3,12 @@ package com.example.examendamfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -20,7 +23,32 @@ public class Formulario extends AppCompatActivity {
         buttonRVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                EditText longitud = findViewById(R.id.txtlongitud);
+                EditText latitud = findViewById(R.id.txtlatitud);
+                EditText marcador = findViewById(R.id.txtmarcador);
+                String mensaje=null;
+
+                if (longitud.getText().toString().length()==0) {
+                    mensaje="* Ingrese la longitud\n";
+                }
+                if (latitud.getText().toString().length()==0) {
+                    mensaje=mensaje+"* Ingrese la latitud\n";
+                }
+                if (marcador.getText().toString().length()==0) {
+                    mensaje=mensaje+"* Ingrese el marcador";
+                }
+
+                if(mensaje==null){
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("geo:<"+longitud.getText().toString()+">,<"+latitud.getText().toString()+">?q=<"+longitud.getText().toString()+">,<"+latitud.getText().toString()+">("+longitud.getText().toString()+")"));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
